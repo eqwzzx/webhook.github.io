8158f3c0e049ecb2744471219e36dd377c56fa60
eqwzzx <eqwzzx@list.ru>
2025-06-09 16:02:13 +0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,14 +3,14 @@ import type { Metadata } from "next"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { AuthProvider } from "@/components/auth-provider"
+import { Analytics } from '@vercel/analytics/next';
 import { Inter } from "next/font/google"
 
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.dev",
+  title: "Webhook Sender",
+  description: "Created by eqwzzx",
 }
 
 export default function RootLayout({
@@ -23,6 +23,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           <AuthProvider>{children}</AuthProvider>
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
